d7e3cc25350a43fab324a076c520275e6f7cf436
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2011-09-28 23:20:57 +0000

    compiled new version of plugin
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs-grails-plugins/grails-ncs-event/trunk@127 87bbd5ad-132b-4bfb-b67c-16c9f46f48d1

--- NcsEventGrailsPlugin.groovy
+++ NcsEventGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class NcsEventGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [ncs-people:0.2]
+    def dependsOn = [ ncsPeople : "0.1 > *", ncsTracking : "1.0 > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
