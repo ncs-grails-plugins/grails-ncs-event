6d5c1bfa60f4bac6030e2f0c0bc7dd327d0b2b08
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-03-27 14:04:32 -0500

    bumping plugin version to build new artifact

--- NcsEventGrailsPlugin.groovy
+++ NcsEventGrailsPlugin.groovy
@@ -1,10 +1,10 @@
 class NcsEventGrailsPlugin {
     // the plugin version
-    def version = "1.1"
+    def version = "1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [ ncsPeople : "0.1 > *", ncsTracking : "0.6 > *"]
+    def dependsOn = [ ncsPeople : "1.0 > *", ncsTracking : "3.2.6 > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
